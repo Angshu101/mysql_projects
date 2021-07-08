bd9e1d77389b6ebc84d34c0f095281d86d326b63
Angshu101 <angshu101.9733@gmail.com>
2021-07-08 08:24:34 +0530

    Update two_basic.sql

--- one_basic/two_basic.sql
+++ one_basic/two_basic.sql
@@ -1,13 +1,13 @@
 /* SHOW DATABASES;
 USE photo_store;
 SELECT DATABASE(); */
-CREATE TABLE customers(
+/* CREATE TABLE customers(
     customers_id INT NOT NULL AUTO_INCREMENT,
     name varchar(30) NOT NULL,
     email varchar(30) NOT NULL DEFAULT 'no email provided',
     amount INT,
     PRIMARY KEY (customers_id)
-);
+); */
 
 /* INSERT INTO customers(name,email,amount)
 VALUES(
@@ -15,11 +15,17 @@ VALUES(
     '1904128@kiit.ac.in',
     '780'
 ); */
-INSERT INTO customers(name, email, amount)
+
+
+/* INSERT INTO customers(name, email, amount)
 VALUES ('hitesh', 'hitesh@lco.dev', 35),
         ('George', 'geo@lco.dev', 45),
         ('hitesh', 'hitesh@gmail.com', 88),
         ('lina', 'lina@gmail.com', 78),
         ('Jimmy', 'jimmy@yahoo.co.in', 54),
          ('lina', 'lina@yahoo.co.in', 35),
-        ('hitesh', 'hitesh@yahoo.co.in', 56);
+        ('hitesh', 'hitesh@yahoo.co.in', 56); */
+
+--TODO: conept of Aliasing
+/* SELECT amount AS purchases from customers; */
+
